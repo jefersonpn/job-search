e6b0afe0d1c8501e0528cd402fb077a9580a636d
Jefersonpn <jeferson.nascimento@gruppogaspari.it>
2023-09-11 12:53:25 +0200

    fixing issue the matchers

--- tests/setup.js
+++ tests/setup.js
@@ -1,8 +1,10 @@
 import { cleanup } from '@testing-library/vue'
-import matchers, { matchers } from '@testing-library/jest-dom/matchers'
+// import { matchers } from '@testing-library/jest-dom/matchers' //The teacher sad to do like this but comes out a error msg. So i found onn the Q&A this solution below witch works
+import * as matchers from '@testing-library/jest-dom/matchers'
 import { expect, afterEach } from 'vitest'
 
 expect.extend(matchers)
+
 afterEach(() => {
   cleanup()
 })
