d57c54e9a119aa335c13f1875fc075a56d2f3cd3
Jefersonpn <jeferson.nascimento@gruppogaspari.it>
2023-09-11 12:52:33 +0200

    Map exercice

---
+++ src/playground.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5]
+const names = ['JEFERSON', 'WELLEN', 'CALEB', 'BENJAMIN']
+
+const squares = numbers.map((number) => number * number)
+
+const lowercaseNames = names.map((name) => name.toLowerCase())
+
+console.log(lowercaseNames)
